50ad1e4657c5af0e2795add2ef76995ad4dfd0c3
Adoozi <138693544+Adoozi@users.noreply.github.com>
2023-07-06 01:01:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-// Variables
 let score = 0;
-let upgradeLevel = 1;
 let upgradeCost = 10;
 
-// Elements
-const scoreElement = document.getElementById('score');
-const upgradeLevelElement = document.getElementById('upgradeLevel');
-const upgradeBtn = document.getElementById('upgradeBtn');
-
-// Update score
-function updateScore() {
-  scoreElement.textContent = score;
+function increaseScore() {
+  score++;
+  document.getElementById("scoreValue").textContent = score;
 }
 
-// Update upgrade level and cost
-function updateUpgrade() {
-  upgradeLevelElement.textContent = upgradeLevel;
-  upgradeBtn.textContent = `Upgrade (Cost: ${upgradeCost})`;
-}
-
-// Click event for the cookie
-const cookie = document.getElementById('cookie');
-cookie.addEventListener('click', function() {
-  score += upgradeLevel;
-  updateScore();
-});
-
-// Click event for the upgrade button
-upgradeBtn.addEventListener('click', function() {
+function buyUpgrade() {
   if (score >= upgradeCost) {
     score -= upgradeCost;
-    upgradeLevel++;
-    upgradeCost += 10;
-    updateScore();
-    updateUpgrade();
+    upgradeCost *= 2;
+    document.getElementById("scoreValue").textContent = score;
+    document.getElementById("upgradeCost").textContent = upgradeCost;
+    // Apply upgrade effect here
   }
-});
-
-// Check score to enable/disable upgrade button
-setInterval(function() {
-  if (score >= upgradeCost) {
-    upgradeBtn.disabled = false;
-  } else {
-    upgradeBtn.disabled = true;
-  }
-}, 100);
-
-// Initialize
-updateScore();
-updateUpgrade();
+}
